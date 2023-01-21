0d2aba36b082ffb30cda068ff4ac69175757f7ae
Ayomide Bakare <bakkareayomideo@gmail.com>
2023-01-21 01:54:29 +0000

    update login page

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,12 +1,17 @@
 import type { ActionArgs, LoaderArgs, MetaFunction } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, Link, useActionData, useSearchParams } from "@remix-run/react";
+import { Form, useActionData, useSearchParams } from "@remix-run/react";
 import * as React from "react";
 
 import { createUserSession, getUserId } from "~/session.server";
 import { verifyLogin } from "~/models/user.server";
 import { safeRedirect, validateEmail } from "~/utils";
 import { Route } from "~/routes.enum";
+import { Button } from "~/ui/Button";
+import { Input } from "~/ui/Input";
+import { Spacing } from "~/ui/Spacing";
+import { useFormValidator } from "~/hooks/useFormValidator";
+import { LoginUserObjectSchema } from "~/schemas/user";
 
 export async function loader({ request }: LoaderArgs) {
   const userId = await getUserId(request);
@@ -22,33 +27,21 @@ export async function action({ request }: ActionArgs) {
   const remember = formData.get("remember");
 
   if (!validateEmail(email)) {
-    return json(
-      { errors: { email: "Email is invalid", password: null } },
-      { status: 400 }
-    );
+    return json({ error: "Email is invalid" }, { status: 400 });
   }
 
   if (typeof password !== "string" || password.length === 0) {
-    return json(
-      { errors: { email: null, password: "Password is required" } },
-      { status: 400 }
-    );
+    return json({ error: "Password is required" }, { status: 400 });
   }
 
   if (password.length < 8) {
-    return json(
-      { errors: { email: null, password: "Password is too short" } },
-      { status: 400 }
-    );
+    return json({ error: "Password is too short" }, { status: 400 });
   }
 
   const user = await verifyLogin(email, password);
 
   if (!user) {
-    return json(
-      { errors: { email: "Invalid email or password", password: null } },
-      { status: 400 }
-    );
+    return json({ error: "Invalid email or password" }, { status: 400 });
   }
 
   return createUserSession({
@@ -69,112 +62,59 @@ export default function LoginPage() {
   const [searchParams] = useSearchParams();
   const redirectTo = searchParams.get("redirectTo") || Route.RECORDS;
   const actionData = useActionData<typeof action>();
-  const emailRef = React.useRef<HTMLInputElement>(null);
-  const passwordRef = React.useRef<HTMLInputElement>(null);
-
-  React.useEffect(() => {
-    if (actionData?.errors?.email) {
-      emailRef.current?.focus();
-    } else if (actionData?.errors?.password) {
-      passwordRef.current?.focus();
-    }
-  }, [actionData]);
+  const validator = useFormValidator(LoginUserObjectSchema);
+  const fields = validator.fields;
 
   return (
-    <div className="flex min-h-full flex-col justify-center">
-      <div className="mx-auto w-full max-w-md px-8">
-        <Form method="post" className="space-y-6">
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Email address
-            </label>
-            <div className="mt-1">
-              <input
-                ref={emailRef}
-                id="email"
-                required
-                autoFocus={true}
-                name="email"
-                type="email"
-                autoComplete="email"
-                aria-invalid={actionData?.errors?.email ? true : undefined}
-                aria-describedby="email-error"
-                className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
-              />
-              {actionData?.errors?.email && (
-                <div className="pt-1 text-red-700" id="email-error">
-                  {actionData.errors.email}
-                </div>
-              )}
-            </div>
-          </div>
-
-          <div>
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Password
-            </label>
-            <div className="mt-1">
-              <input
-                id="password"
-                ref={passwordRef}
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                aria-invalid={actionData?.errors?.password ? true : undefined}
-                aria-describedby="password-error"
-                className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
-              />
-              {actionData?.errors?.password && (
-                <div className="pt-1 text-red-700" id="password-error">
-                  {actionData.errors.password}
-                </div>
-              )}
-            </div>
-          </div>
-
-          <input type="hidden" name="redirectTo" value={redirectTo} />
-          <button
-            type="submit"
-            className="w-full rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
-          >
-            Log in
-          </button>
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <input
-                id="remember"
-                name="remember"
-                type="checkbox"
-                className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
-              />
-              <label
-                htmlFor="remember"
-                className="ml-2 block text-sm text-gray-900"
-              >
-                Remember me
-              </label>
-            </div>
-            <div className="text-center text-sm text-gray-500">
-              Don't have an account?{" "}
-              <Link
-                className="text-blue-500 underline"
-                to={{
-                  pathname: Route.REGISTER,
-                  search: searchParams.toString(),
-                }}
-              >
-                New user
-              </Link>
-            </div>
-          </div>
-        </Form>
+    <Form
+      method="post"
+      className="mx-auto mt-40 w-full max-w-md rounded-lg px-6 py-4 shadow-lg shadow-gray-200 ring-1 ring-gray-200"
+    >
+      <h4 className="my-0 text-lg font-bold text-gray-900">Log in</h4>
+      <Spacing />
+      <input type="hidden" name="redirectTo" value={redirectTo} />
+      <div className="grid grid-cols-2 gap-4">
+        <Input
+          autoFocus
+          size="sm"
+          type="email"
+          label="Email address"
+          {...fields.email}
+        />
+        <Input
+          size="sm"
+          type="password"
+          label="Password"
+          {...fields.password}
+        />
       </div>
-    </div>
+      <Spacing vertical={actionData?.error ? "2" : "4"} />
+      <div className="flex flex-col items-center justify-center">
+        {actionData?.error && (
+          <>
+            <span className="text-sm text-red-400">{actionData?.error}</span>
+            <Spacing />
+          </>
+        )}
+        <Button size="sm" type="submit" disabled={validator.isInvalid}>
+          Log in
+        </Button>
+        <Spacing />
+        <div className="flex items-center gap-2">
+          <input
+            id="remember"
+            name="remember"
+            type="checkbox"
+            className="h-4 w-4 rounded border-gray-300 text-purple-600 transition focus:ring-purple-500"
+          />
+          <label
+            htmlFor="remember"
+            className="block text-xs font-normal text-gray-500"
+          >
+            Remember me
+          </label>
+        </div>
+      </div>
+    </Form>
   );
 }
--- app/schemas/user.ts
+++ app/schemas/user.ts
@@ -1,7 +1,13 @@
+import { z } from "zod";
 import { UserObjectSchema } from "~/generated/schemas";
 
 export const CreateUserObjectSchema = UserObjectSchema.omit({
   id: true,
   createdAt: true,
   updatedAt: true,
+});
+
+export const LoginUserObjectSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
 });
